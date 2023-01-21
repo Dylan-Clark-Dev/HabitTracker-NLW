597bfbb716c8e176a2454a82844d5d7d6712ea93
Dylan Clark <dylanclarkdev2209@gmail.com>
2023-01-21 18:53:35 -0300

    Adiciona o Dia ao clicar no botão

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 const form = document.querySelector('#form-habits')
 const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
 
-const data = {
-  run: ['01-01', '01-02', '01-03', '01-06', '01-07','01-08'],
-  food: ['01-06'],
-  water: ['01-02'],
+button.addEventListener('click', add)
+
+function add() {
+
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+  
+  if(dayExists) {
+    alert("Dia já incluso")
+    return
+  }
+
+  nlwSetup.addDay(today)
 }
 
-nlwSetup.setData(data)
-nlwSetup.load()
+//const data = {
+//  run: ['01-01', '01-02', '01-03', '01-06', '01-07','01-08'],
+// food: ['01-06'],
+//  water: ['01-02'],
+//}
+
+//nlwSetup.setData(data)
+//nlwSetup.load()
